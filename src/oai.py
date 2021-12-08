77886321db19e6b7f36f49c7a06038bcfb319312
esteng <elias.stengel@gmail.com>
2021-12-08 11:11:49 -0500

    ignore api key

--- src/oai.py
+++ src/oai.py
@@ -2,22 +2,23 @@ import os
 import openai
 import requests
 import json 
+from typing import List
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-def run_prompt(text): 
+def run_prompt(text, kwargs): 
     prompt = {
-      #"prompt": """Q: Is a dog biting a cat eager to bite, or slow to bite?\nEager to bite
-      #Q: Is a doll with a blindfold on easy to see, or hard to see?\nA: Easy to see
-      #Q: Is a person with a blindfold on easy to see, or hard to see?\nA: """,
       "prompt": text,
-      "max_tokens": 5,
-      "temperature": 1,
-      "top_p": 1,
-      "n": 1,
-      "logprobs": 5, 
-      "stop": "\n"
     }
+    #prompt = {"prompt": text,
+    #        "max_tokens": 5,
+    #     "temperature": 1,
+    #     "top_p": 1,
+    #     "n": 1,
+    #     "logprobs": 5, 
+    #     "stop": "\n"}
+    #prompt.update(kwargs) 
+    #print(prompt)
 
     r = requests.get("https://api.openai.com/v1/engines/davinci/completions/browser_stream",
       headers={
@@ -47,14 +48,10 @@ def run_prompt(text):
     text = " ".join([x['choices'][0]['text'] for x in responses])
     return text 
 
-#openai.organization = "org-ip1LnqT1MhUutdy87CyMO3mw"
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
 class Prompt:
     def __init__(self, 
                  context: List[str], 
-                 prompt: [str], 
+                 prompt: str, 
                  context_sep = "\n"): 
         self.context = context 
         self.prompt = prompt 
@@ -78,11 +75,35 @@ text2b =  """Q: Is a doll wearing a blindfold on easy to see, or hard to see?\nA
 
 
 
+#for prompt in [text1a, text2a, text3a, text1b, text2b]: 
+#    resp = run_prompt(prompt) 
+#
+#    print(f"PROMPT:\n{prompt}")
+#    print(f"RESPONSE:\n-----------{resp}")
 
-for prompt in [text1a, text2a, text3a, text1b, text2b]: 
-    resp = run_prompt(prompt) 
+#prompt1c = Prompt(context=["Two people named Paul and George are playing. Paul promises George to stand on a hill."], prompt = "Q: Who stood on the hill?") 
+#prompt2c = Prompt(context=["Two people named Paul and George are playing. Paul forces George to stand on a hill."], prompt = "Q: Who stood on the hill?") 
+#text1c = str(prompt1c) 
+#text2c = str(prompt2c) 
+#
+#for prompt in [text1c, text2c]: 
+#    resp = run_prompt(prompt) 
+#
+#    print(f"PROMPT:\n{prompt}")
+#    print(f"RESPONSE:\n-----------{resp}")
 
-    print(f"PROMPT:\n{prompt}")
-    print(f"RESPONSE:\n-----------{resp}")
-
-
+#prompt1d = Prompt(context=["Mary dreamt that Paul had left."], prompt = "Q: Did Paul leave?") 
+#prompt2d = Prompt(context=["Two people named Paul and George are playing. Paul forces George to stand on a hill."], prompt = "Q: Who stood on the hill?") 
+#text1d = str(prompt1d) 
+#kwargs = {"max_tokens": 5,
+#         "temperature": 1,
+#         "top_p": 1,
+#         "n": 1,
+#         "logprobs": 5, 
+#         "stop": "\n"}
+#
+#for prompt in [text1d]: 
+#    resp = run_prompt(prompt, kwargs) 
+#
+#    print(f"PROMPT:\n{prompt}")
+#    print(f"RESPONSE:\n-----------{resp}")
