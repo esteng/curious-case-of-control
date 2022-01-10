e558411c14cd144811c156066085ae71b265f9bb
esteng <elias.stengel@gmail.com>
2022-01-10 10:33:29 -0500

    results and check in src

--- src/vis.py
+++ src/vis.py
@@ -1,22 +1,33 @@
+import re 
+import json 
+import pathlib
+from numpy.core.shape_base import _arrays_for_stack_dispatcher 
+
 import seaborn as sns 
 from matplotlib import pyplot as plt 
 import pandas as pd 
 import numpy as np 
-import json 
-import pathlib 
-from experiment import Experiment
 
+from experiment import Experiment
 from metrics import accuracy_report
 
-import re 
+def get_palette(): 
+    return {"gpt3": '#0173b2', "gpt-neo-1.3b": '#de8f05', "gpt-neo-2.7b": '#029e73', 
+            "gpt-j": '#d55e00', "jurassic-large": '#cc78bc', "jurassic-jumbo": '#ca9161', 
+            "t5": '#fbafe4', "t0": '#949494'} 
+
 def barplot(csv_groups, level, x_name, hue_name, title=None, ax=None, filtered = False, ignore_first_only = False):
     # consolidate data from different csvs 
     all_dfs = {g:[] for g in csv_groups.keys()}
     for group, csvs in csv_groups.items():
         for csv in csvs:
-            df = pd.read_csv(csv)
-            # print(csv, len(df))
-            all_dfs[group].append(df)
+            try:
+                df = pd.read_csv(csv)
+                # print(csv, len(df))
+                all_dfs[group].append(df)
+            except FileNotFoundError:
+                print(f"Not found: {csv}")
+
 
     reports = {g: [accuracy_report(df, ignore_first_only, total_only=True) for df in dfs] for g, dfs in all_dfs.items()}
     # data = [report[level] for report in reports]
@@ -45,8 +56,11 @@ def barplot(csv_groups, level, x_name, hue_name, title=None, ax=None, filtered =
                     acc = float(v[0])
                 type_name = "total"
                 df_to_plot = df_to_plot.append({"model": model_name, "acc": acc, "type": type_name, "group": group}, ignore_index=True)
+    palette = get_palette()
 
-    g = sns.catplot(data = df_to_plot, kind='bar', x = x_name, y = 'acc', hue = hue_name, palette="colorblind", col="group")
+    order = [ "gpt-neo-1.3b", "gpt-neo-2.7b", "gpt-j", "gpt3", "jurassic-large","jurassic-jumbo","t5", "t0"]
+
+    g = sns.catplot(data = df_to_plot, kind='bar', x = x_name, y = 'acc', hue = hue_name, palette=palette, col="group", hue_order=order)
     g.set(ylim=(0, 1.0))
 
     # g.ax.set_ylim(top=1.0, bottom=0.0)
@@ -71,10 +85,22 @@ def recompute(csvs,
               correct_idx=None):
     prompt=None
     wrapper_fxn=None
+    accuracy_reports = {}
+
     for csv, name in zip(csvs, names): 
-        exp  = Experiment(name, exp_name, prompt, wrapper_fxn, 1, None)
-        filename = pathlib.Path(csv).name
-        exp.recover(csv)
-        exp.recompute(nicknames, use_action=use_action, use_verb=use_verb, correct_idx=correct_idx)
-        df = exp.format_results()
-        df.to_csv(f"../{results_path}/{filename}")
+        try:
+            exp  = Experiment(name, exp_name, prompt, wrapper_fxn, 1, None)
+            filename = pathlib.Path(csv).name
+            exp.recover(csv)
+            exp.recompute(nicknames, use_action=use_action, use_verb=use_verb, correct_idx=correct_idx)
+            df = exp.format_results()
+            report = accuracy_report(df, total_only=True)
+            accuracy_reports[name] = report['total']
+            try:
+                df.to_csv(f"../{results_path}/{filename}")
+            except FileNotFoundError:
+                print(f"FileNotFound: ../{results_path}/{filename}")
+        except FileNotFoundError:
+            print(f"FileNotFound: {csv}")
+        
+    return accuracy_reports
